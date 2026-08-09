4af791640ebea19ad8bf225f67922cb94a870951
abhassomkuwar <abhassomkuwar7@gmail.com>
2026-08-09 10:49:46 +0530

    add Iterator, for-each loop, and forEach method support for traversing LinkedList in CollectionDemo

--- CollectionDemo.java
+++ CollectionDemo.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Iterator;
 
 public class CollectionDemo {
 
@@ -9,13 +10,28 @@ public class CollectionDemo {
         lst.add("Microsoft");
         lst.add("Google");
         lst.addFirst("Apple");
-        lst.addLast("Apache");
-        lst.add(2, "Oracle");
+        lst.addLast("Apache");  
+        lst.add(2, "Oracle"); 
 
-        System.out.println("--- Traversing using for loop"); 
+        System.out.println("Traversing using for loop"); 
 
         for (int i = 0; i < lst.size(); i++) {
             System.out.println(lst.get(i)); 
         }
+
+        System.out.println("Traversing using Iterator");
+        Iterator<String> itr = lst.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+
+        System.out.println("Traversing using for-each");
+        for(String s : lst) {
+            System.out.println(s);
+
+
+        System.out.println("Traversing using for-each method");
+        lst.forEach(e -> System.out.println(e));
+        }    
+        }
     }
-} 
+}  
